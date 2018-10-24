28f74282f712fb932918aa8cc94f03c29c4eb74a
Daniel Córdova A <danesc87@gmail.com>
2018-10-24 16:46:29 -0500

    Add UnitTest and change framework to JUnit5

--- micronaut-sqlite-crud/src/test/kotlin/database/DBUtilsTest.kt
+++ micronaut-sqlite-crud/src/test/kotlin/database/DBUtilsTest.kt
@@ -3,29 +3,50 @@ package database
 import com.nanobytes.crud.database.DBUtils
 import ninja.sakib.pultusorm.core.PultusORM
 import ninja.sakib.pultusorm.core.enableDebugMode
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.lang.Exception
 
 /**
  * Class that test if DBUtils works properly
  * @author Daniel Córdova A.
  */
 class DBUtilsTest {
-    private val dbUtils: DBUtils = DBUtils
-    private lateinit var testORM: PultusORM
 
-    @Before
-    fun setUp() {
-        enableDebugMode(true)
-        testORM = PultusORM("mscDB.db", System.getProperty("user.dir"))
+    private val dbUtils: DBUtils = DBUtils
+
+    companion object {
+
+        private lateinit var testORM: PultusORM
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            enableDebugMode(true)
+            testORM = PultusORM("mscDB.db", System.getProperty("user.dir"))
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterTest() {
+            val fileNameWithPath: String = System.getProperty("user.dir").plus("mscDB.db")
+            try {
+                val file: File = File(fileNameWithPath)
+                file.delete()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 
     @Test
     fun shouldCreateANewDBOrConnection(){
         dbUtils.initOrCreate()
-        val ormInstance = dbUtils.pultusORM!!
-        Assert.assertEquals(
+        val ormInstance = dbUtils.pultusORM
+        Assertions.assertEquals(
                 ormInstance.to(DEFAULT_BUFFER_SIZE).second,
                 testORM.to(DEFAULT_BUFFER_SIZE).second
         )
--- micronaut-sqlite-crud/src/test/kotlin/service/SchoolServiceTest.kt
+++ micronaut-sqlite-crud/src/test/kotlin/service/SchoolServiceTest.kt
@@ -3,28 +3,66 @@ package service
 import com.nanobytes.crud.database.DBUtils
 import com.nanobytes.crud.models.School
 import ninja.sakib.pultusorm.core.PultusORM
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.lang.Exception
 
 /**
  * Class that test if School Service works properly
  */
 class SchoolServiceTest {
 
-    private lateinit var pultusORM: PultusORM
-    private val school: School = School()
+    companion object {
 
-    @Before
-    fun setUp() {
-        DBUtils.initOrCreate()
-        pultusORM = DBUtils.pultusORM
-        school.schoolName = "Test"
+        private lateinit var pultusORM: PultusORM
+        private val testSchool: School = School()
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            DBUtils.initOrCreate()
+            pultusORM = DBUtils.pultusORM
+            testSchool.schoolName = "Engineering School"
+            pultusORM.save(testSchool)
+        }
+
+        @AfterAll
+        @JvmStatic
+        fun afterTest() {
+            val fileNameWithPath: String = System.getProperty("user.dir").plus("mscDB.db")
+            try {
+                val file: File = File(fileNameWithPath)
+                file.delete()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
+
+
 
     @Test
     fun shouldReturnTrueWhileSaveNewSchool() {
-        val result: Boolean = pultusORM.save(school)
-        Assert.assertTrue(result)
+        val result: Boolean = pultusORM.save(testSchool)
+
+        // Save operation must be false because School Name is Unique
+        // and was saved on setUp() function
+        Assertions.assertFalse(result)
+    }
+
+    @Test
+    fun shouldReturnAListWithSchoolNames() {
+        val expected: List<School> = listOf(testSchool)
+        val schoolList:MutableList<Any> = pultusORM.find(School())
+        var actual: MutableList<School> = mutableListOf()
+        schoolList.forEach { actual.add(it as School) }
+
+        Assertions.assertEquals(
+                expected.get(0).schoolName,
+                actual.get(0).schoolName
+        )
     }
 }
