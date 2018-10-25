ba8a7da47000944354efbe0a9e261771c34b58a7
Daniel Córdova A <danesc87@gmail.com>
2018-10-25 17:25:08 -0500

    Add UnitTest and Service for save new career

--- micronaut-sqlite-crud/src/main/kotlin/com/nanobytes/crud/service/CareerService.kt
+++ micronaut-sqlite-crud/src/main/kotlin/com/nanobytes/crud/service/CareerService.kt
@@ -1,8 +1,25 @@
 package com.nanobytes.crud.service
 
+import com.nanobytes.crud.database.DBUtils
+import com.nanobytes.crud.models.Career
+import ninja.sakib.pultusorm.core.PultusORM
+import java.lang.IndexOutOfBoundsException
+
 /**
  * Class that has all functions for Career object
  * @author Daniel Córdova A.
  */
 object CareerService {
+
+    private val pultusORM: PultusORM = DBUtils.pultusORM
+
+    fun saveNewCareer(career: Career): Boolean {
+        val schoolId: Int = career.schoolId
+        return try {
+            SchoolService.getSchoolById(schoolId)
+            pultusORM.save(career)
+        } catch (e: IndexOutOfBoundsException) {
+            false
+        }
+    }
 }
