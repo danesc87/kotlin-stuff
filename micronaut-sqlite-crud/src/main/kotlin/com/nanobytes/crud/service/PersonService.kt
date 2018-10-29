cedc1d01a10df833078f253fad4323baae41c99f
Daniel Córdova A <danesc87@gmail.com>
2018-10-29 17:15:00 -0500

    Add UnitTest and function for POST request for Person

--- micronaut-sqlite-crud/src/main/kotlin/com/nanobytes/crud/service/PersonService.kt
+++ micronaut-sqlite-crud/src/main/kotlin/com/nanobytes/crud/service/PersonService.kt
@@ -1,8 +1,18 @@
 package com.nanobytes.crud.service
 
+import com.nanobytes.crud.database.DBUtils
+import com.nanobytes.crud.models.Person
+import ninja.sakib.pultusorm.core.PultusORM
+
 /**
  * Class that has all functions for Person object
  * @author Daniel Córdova A.
  */
 object PersonService {
+
+    private val pultusORM: PultusORM = DBUtils.pultusORM
+
+    fun saveNewPerson(person: Person): Boolean {
+        return pultusORM.save(person)
+    }
 }
--- micronaut-sqlite-crud/src/test/kotlin/service/PersonServiceTest.kt
+++ micronaut-sqlite-crud/src/test/kotlin/service/PersonServiceTest.kt
@@ -1,8 +1,43 @@
 package service
 
+import com.nanobytes.crud.database.DBUtils
+import com.nanobytes.crud.models.Person
+import com.nanobytes.crud.service.PersonService
+import ninja.sakib.pultusorm.core.PultusORM
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+
 /**
  * Class that test if Person Service works properly
  * @author Daniel Córdova A.
  */
 class PersonServiceTest {
+
+    companion object {
+
+        private lateinit var testORM: PultusORM
+        private val testPerson: Person = Person()
+        private val personService: PersonService = PersonService
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            DBUtils.initOrCreate("testDB.db", System.getProperty("user.dir"))
+            testORM = DBUtils.pultusORM
+            testPerson.dni = "0123456789-A1"
+            testPerson.name = "John"
+            testPerson.lastName = "Doe"
+            testORM.save(testPerson)
+        }
+    }
+
+    @Test
+    fun shouldReturnTrueWhileSaveNewPerson() {
+        val result: Boolean = personService.saveNewPerson(testPerson)
+
+        // Save operation must be false because Person DNI is Unique
+        // and was saved on setUp() function
+        Assertions.assertFalse(result)
+    }
 }
